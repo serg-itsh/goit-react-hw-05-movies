fcd5e59c2324c60c64b198c5538e576386632db3
Sergey Shevchenko <sergeishevchenko111@gmail.com>
2022-08-09 19:28:27 +0300

    Внесення змін

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,16 @@ import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router';
 import { Loader } from './Loader/Loader';
 import { Navigation } from './Navigation/Navigation';
+// import { MovieDetails, Outlet } from 'react-router-dom';
 
 const Home = lazy(() => import('pages/Home' /* webpackChunkName: "Home" */));
 const Movies = lazy(() =>
   import('pages/Movies' /* webpackChunkName: "Movies" */)
 );
 const MovieDetails = lazy(() =>
-  import('./MovieDetails/MovieDetails' /* webpackChunkName: "MoviesDetails" */)
+  import(
+    '../pages/MovieDetails/MovieDetails' /* webpackChunkName: "MoviesDetails" */
+  )
 );
 const Cast = lazy(() => import('./Cast/Cast' /* webpackChunkName: "Cast" */));
 const Reviews = lazy(() =>
@@ -23,59 +26,153 @@ const NotFoundView = lazy(() =>
 export const App = () => {
   return (
     <div>
-      <Navigation />
+      {/* <Navigation /> */}
+      {/* <Outlet /> */}
+
       <Routes>
         <Route
-          index
+          path="/"
           element={
             <Suspense fallback={<Loader />}>
-              <Home />
-            </Suspense>
-          }
-        ></Route>
-        <Route
-          path="/movies"
-          element={
-            <Suspense fallback={<Loader />}>
-              <Movies />
-            </Suspense>
-          }
-        ></Route>
-        <Route
-          path="/movies/:movieId"
-          element={
-            <Suspense fallback={<Loader />}>
-              <MovieDetails />
+              {' '}
+              <Navigation />{' '}
             </Suspense>
           }
         >
           <Route
-            path="cast"
+            index
             element={
               <Suspense fallback={<Loader />}>
-                <Cast />
+                {' '}
+                <Home />{' '}
               </Suspense>
             }
           ></Route>
           <Route
-            path="reviews"
+            path="/movies"
             element={
               <Suspense fallback={<Loader />}>
-                <Reviews />
+                {' '}
+                <Movies />
               </Suspense>
             }
           ></Route>
+
+          <Route
+            path="/movies/:movieId"
+            element={
+              <Suspense fallback={<Loader />}>
+                <MovieDetails />{' '}
+              </Suspense>
+            }
+          >
+            <Route
+              path="cast"
+              element={
+                <Suspense fallback={<Loader />}>
+                  {' '}
+                  <Cast />{' '}
+                </Suspense>
+              }
+            ></Route>
+            <Route
+              path="reviews"
+              element={
+                <Suspense fallback={<Loader />}>
+                  <Reviews />
+                </Suspense>
+              }
+            ></Route>
+          </Route>
+
+          <Route
+            path="*"
+            element={
+              <Suspense fallback={<Loader />}>{<NotFoundView />}</Suspense>
+            }
+          ></Route>
         </Route>
-        <Route
-          path="*"
-          element={
-            <Suspense fallback={<Loader />}>
-              <NotFoundView />
-            </Suspense>
-          }
-        />
       </Routes>
       <ToastContainer />
     </div>
   );
 };
+
+//
+//  return (
+//    <div>
+//      <Navigation />
+
+//      <Routes>
+//        <Route
+//          index
+//          element={
+//            <Suspense fallback={<Loader />}>
+//              {' '}
+//              <Home />{' '}
+//            </Suspense>
+//          }
+//        ></Route>
+//        <Route
+//          path="/movies"
+//          element={
+//            <Suspense fallback={<Loader />}>
+//              {' '}
+//              <Movies />
+//            </Suspense>
+//          }
+//        ></Route>
+
+//        <Route
+//          path="/movies/:movieId"
+//          element={
+//            <Suspense fallback={<Loader />}>
+//              <MovieDetails />{' '}
+//            </Suspense>
+//          }
+//        >
+//          <Route
+//            path="cast"
+//            element={
+//              <Suspense fallback={<Loader />}>
+//                {' '}
+//                <Cast />{' '}
+//              </Suspense>
+//            }
+//          ></Route>
+//          <Route
+//            path="reviews"
+//            element={
+//              <Suspense fallback={<Loader />}>
+//                <Reviews />
+//              </Suspense>
+//            }
+//          ></Route>
+//        </Route>
+
+//        <Route
+//          path="*"
+//          element={<Suspense fallback={<Loader />}>{<NotFoundView />}</Suspense>}
+//        ></Route>
+//      </Routes>
+//      <ToastContainer />
+//    </div>
+//  );
+
+// export const App = () => {
+//   return (
+//     <Suspense fallback={<div>Loading...</div>}>
+//       <Routes>
+//         <Route path="/" element={<Navigation />}>
+//           <Route index element={<Home />} />
+//           <Route path="movies" element={<Movies />} />
+//           <Route path="movies/:movieId" element={<MovieDetails />}>
+//             <Route path="cast" element={<Cast />} />
+//             <Route path="reviews" element={<Reviews />} />
+//           </Route>
+//           <Route path="*" element={<NotFound />} />
+//         </Route>
+//       </Routes>
+//     </Suspense>
+//   );
+// };
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,16 +1,20 @@
 import { Link, List, Nav } from './Naigation.styled.js';
+import { Outlet } from 'react-router-dom';
 
 export const Navigation = () => {
   return (
-    <Nav>
-      <List>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/movies">Movies</Link>
-        </li>
-      </List>
-    </Nav>
+    <>
+      <Nav>
+        <List>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/movies">Movies</Link>
+          </li>
+        </List>
+      </Nav>
+      <Outlet />
+    </>
   );
 };
